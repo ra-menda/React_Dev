251956bda437dfea97d99557a506b56d05ec392c
ra-men <ramenda1217@gmail.com>
2020-04-30 23:55:24 +0900

    コンポーネントと props までやってみた

--- helloworld/src/index.js
+++ helloworld/src/index.js
@@ -1,24 +1,65 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function formatName(user) {
-    return user.firstname + " " + user.lastName;
+
+
+  function formatDate(date) {
+    return date.toLocaleDateString();
+  }
+
+
+function Avatar(props){
+    return (
+        <img className="Avatar"
+        src={props.user.avatarUrl}
+        alt={props.user.name}
+        />
+    );
 }
 
-const user = {
-    firstname: "ramen",
-    lastName: "Nazomaster"
-};
+function UserInfo(props){
+    return (
+        <div className="UserInfo">
+            <Avatar user={props.user} />
+            <div className="UserInfo-name">
+                {props.user.name}
+            </div>
+        </div>
+    )
+}
 
-const element =(
-    <h1>
-        Hello,{formatName(user)}!
-    </h1>
+function Comment(props) {
+        return (
+        <div className="Comment">
+            <UserInfo user={props.author} />
+            <div className="Comment-text">
+              {props.text}
+            </div>
+            <div className="Comment-date">
+              {formatDate(props.date)}
+            </div>
+          </div>
+        );
+
+}
+const comment = {
+    date: new Date(),
+    text: 'I hope you enjoy learning React!',
+    author: {
+      name: 'Hello Kitty',
+      avatarUrl: 'https://placekitten.com/g/64/64',
+    },
+  };
+  
+ReactDOM.render(
+    <Comment
+        date={comment.date}
+        text={comment.text}
+        author={comment.author}
+        />,
+    document.getElementById('root')
 );
 
 
-ReactDOM.render(
-   element,
-    document.getElementById('root'),
-    
-  );
+
+//setInterval(Comment,1000);
